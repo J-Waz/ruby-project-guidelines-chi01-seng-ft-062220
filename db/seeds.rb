aea8dfcb0a31803345c9f877f5fd1c87d7e41df0
matthewlawrenceklein <mtt.l.kln@gmail.com>
2020-07-07 10:51:50 -0500

    built out user favorites methods

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,3 +14,11 @@ Favorite.create(user_id: 1, match_id:1)
 Favorite.create(user_id: 1, match_id:2)
 Favorite.create(user_id: 4, match_id:1)
 Favorite.create(user_id: 4, match_id:4)
+
+
+user1 = User.create(name: "jacob")
+match1 = Match.create(home_team: "weasels", away_team: "birds", location: "florida")
+match2 = Match.create(home_team: "cats", away_team: "birds", location: "florida")
+match3 = Match.create(home_team: "bears", away_team: "birds", location: "florida")
+fav1 = Favorite.create(user_id: user1.id, match_id: match1.id)
+fav2 = Favorite.create(user_id: user1.id, match_id: match2.id)
